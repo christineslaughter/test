05314e0ebde98b217e31405522bb090ac2ed2dd6
christineslaughter <cslaughter93@gmail.com>
2016-09-26 09:24:21 -0700

    This is a test.

---
+++ test.R
@@ -0,0 +1,5 @@
+print("Hello World!")
+install.packages("maps")
+system.file(package="maps")
+library(maps)
+packrat:snapshot() # this is for a collaborator to have access to the same forms of packages that you have 
